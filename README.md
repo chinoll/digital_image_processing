87080e213cdce24a9503bdbdfe733b80ace4ef35
chinoll <chinoll@chinoll.org>
2021-11-14 21:39:38 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # 数字图像处理笔记及其算法实现
-temp.jpg &nbsp;示例图片
+
+sample.jpg &nbsp;示例图片
+
+sample_gray.jpg &nbsp;灰度图
+
+## gray/ &nbsp;&nbsp;灰度变换算法
+
+inversion.py &nbsp;&nbsp; 反色算法
+
+histogram_equalization.py &nbsp;&nbsp; 直方图均衡化算法
--- gray/inversion.py
+++ gray/inversion.py
@@ -2,16 +2,17 @@ import numpy as np
 from PIL import Image
 import sys
 sys.path.append("..")
+sys.path.append(".")
 from utils import *
 
 def image_inversion(img,L):
     """
     反转图像的灰度值，计算公式为 s = L - img
     """
-    m = L - img.astype(np.float)
+    m = (L - 1) - img.astype(np.float)
     m = normalize_image(m)
     return Image.fromarray(m)
 
-img = get_examples_image()
-img = image_inversion(img,200)
+img = get_examples_image(3)
+img = image_inversion(img,256)
 img.show()
