ddfd4fd010180404f20d9006ef94156eff5eade9
chinoll <chinoll@chinoll.org>
2021-11-20 23:00:10 +0800

    update laplace

--- spatial_filtering/laplace_kernel.py
+++ spatial_filtering/laplace_kernel.py
@@ -7,11 +7,12 @@ from kernel import *
 from conv import *
 
 if __name__ == "__main__":
-    s = visualization(1,2)
+    s = visualization(1,3)
     #拉普拉斯核
     kernel = laplace_kernel()
     img = get_examples_image(3)
     s.append_img(img)
     img1 = convolve3d(img,kernel)
+    s.append_img(img1)
     s.append_img(img - img1)
     s.show()
